d189276e54f5bdba06143cf04b008cb8cb0d189c
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2023-07-23 17:42:26 +0200

    Update a time

--- src/app.js
+++ src/app.js
@@ -1,16 +1,41 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function dispalyTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let conditionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   conditionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.date.time * 1000);
 }
+
 let apiKey = "7adbfd2oe230496b6bf0deac7aetb83f";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Zagreb&key=7adbfd2oe230496b6bf0deac7aetb83f&units=metric`;
 
