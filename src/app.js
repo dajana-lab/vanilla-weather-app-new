3334b374c753de7d79bc0481dd52b1b5e4130869
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2023-07-23 17:27:16 +0200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function dispalyTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "7adbfd2oe230496b6bf0deac7aetb83f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Zagreb&key=7adbfd2oe230496b6bf0deac7aetb83f&units=metric`;
 
+axios.get(apiUrl).then(dispalyTemperature);
